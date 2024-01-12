f202f9d1126d848f6bd5cf492e02099a2a3d1282
Claire Adkins <cadkins24@students.polytechnic.org>
2024-01-12 14:36:19 -0800

    made working quad eq

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -11,15 +11,14 @@ public class QuadraticSolver{
         double c = input.nextDouble();
 
         double descriminant = ((b*b) - (4*a*c));
-        System.out.println(descriminant);
         if(descriminant > 0) {
         double xValue1 = (-b + Math.sqrt(descriminant)) / (2*a);
         double xValue2 = (-b - Math.sqrt(descriminant)) / (2*a);
         System.out.println("Your answers are: (" + xValue1 + ", " + xValue2 + ")");
-        } if(descriminant == 0) {
+        } else if(descriminant == 0) {
             double xValue3 = (-b / (2*a));
             System.out.println(xValue3);
-        } else {
+        } else{
             System.out.println("no");
         }
      }
